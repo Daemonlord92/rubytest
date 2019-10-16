763b8d252d94e639f137b1e49856d9733a44d6ee
Zac Sparkman <zachsparkmanbusi@gmail.com>
2019-10-15 23:46:05 -0500

    Now, Soon, Later

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-# RUBY TEST
+# Now, Soon, Later
 ---
-This is just a test repo for my own experience using GitHub  
-with Ruby.
+Need some help with this project.
+I'm trying to create a loop that allows the user
+to continue to add as many entries as they like
+into a specified array and have the program store
+the new entries into the array consecutively.
+
+For some reason my while loop is not restarting
+when the user chooses to add another entry and
+the program continues to the next line and prompts 
+"Would you like to view your tasks?"
+
+Not sure where I'm going wrong here. Would appreciate
+some guidance on this. Thanks in advance - Z
 
--- newproject.rb
+++ newproject.rb
@@ -1,12 +1,44 @@
-istall = false
-ismale = false
+now = Array.new
+soon = Array.new
+later = Array.new
+addentry = true
 
-if ismale and istall
-  puts "You are a tall male"
-elsif ismale and !istall
-  puts "You are a short male"
-elsif !ismale and istall
-  puts "You are a tall female"
+while addentry == true
+  puts "Please select a time frame: "
+  puts "1. Now"
+  puts "2. Soon"
+  puts "3. Later"
+
+  choice = gets.chomp.to_i()
+
+  if choice == 1
+    puts "Please enter a task for Now: "
+    now[0] = gets.to_s
+  elsif choice == 2
+    puts "Please enter a task for Soon: "
+    soon[0] = gets.to_s
+  elsif choice == 3
+    puts "Please enter a task for Later: "
+    later[0] = gets.to_s
+  else
+    puts "Invalid Entry. Please try again."
+  end
+
+  puts "Add another entry? (Y/N)"
+  addentry = gets.to_s
+  if addentry == "Y" or addentry == "y"
+    addentry == true
+  elsif addentry == "N" or addentry == "n"
+    addentry == false
+  end
+end
+
+puts "Would you like to view your tasks? (Y/N)"
+answer = gets.chomp.to_s
+
+if answer == "Y" or answer == "y"
+  print "Now Tasks: "
+  puts now
 else
-  puts "You are a short female"
+  puts "Thank You."
 end
